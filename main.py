e51b2cadec576c743982c05c40f48fd0fde4c3ab
Meera Sundar <mirage.sundial@gmail.com>
2026-07-02 13:10:27 +0530

    change model name

--- main.py
+++ main.py
@@ -5,12 +5,11 @@ import streamlit as st
 load_dotenv()
 
 def llm_call(number, topic):
-    gemini_model = ChatGoogleGenerativeAI(model= "gemini-1.5-flash-latest")
+    gemini_model = ChatGoogleGenerativeAI(model= "gemini-3.1-flash-lite")
 
     # Prompt template
     tweet_template = "Give me {number} tweets on {topic}"
     tweet_prompt = PromptTemplate(template=tweet_template, input_variables = ['number', 'topic'])
-    #tweet_template.format(number=5, topic="Fighter Jets of World War 2")
     # LLM Chain
     tweet_chain = tweet_prompt | gemini_model
     response = tweet_chain.invoke({"number": number, "topic": topic})
